129fd1b4a9e972ceb0e8b5097f5f82cf77f37866
Jaffe <jaffe75@gmail.com>
2013-06-07 13:47:18 -0700

    having padding issues

--- js/msscript.js
+++ js/msscript.js
@@ -5,16 +5,16 @@ jQuery(document).ready(function(){
 function getFontSize(name){
         var $numChar = name.length;
         if (($numChar >= 1) && ($numChar < 10)) {
-                return "font-size : 24px;";
+                return "font-size : 24px";
             }
             else if (($numChar >= 10) && ($numChar < 12)) {
-                return "font-size : 18px; ";
+                return "font-size : 18px";
             }
             else if (($numChar >= 12) && ($numChar < 30)) {
-                return "font-size : 14px; ";
+                return "font-size : 14px";
             }
             else if (($numChar >= 30) && ($numChar < 40)) {
-                return "font-size : 11px ; ";
+                return "font-size : 11px";
             }
             else {
                 return "font-size : 12px";
@@ -44,7 +44,7 @@ function get_random_class() {
             for(var i in result ) {
                 var $length = getFontSize(result[i].name);
                 $(".repository-thumbs").append(
-                    "<div class='repo-single-thumb'><h2><a href='" + result[i].html_url + "' target='_blank' style='" + $length + "'>" +
+                    "<div class='repo-single-thumb'><h2><a href='" + result[i].html_url + "' target='_blank' style='" + $length + ";'>" +
                     result[i].name + "</a></h2></div>"
                 );
                
